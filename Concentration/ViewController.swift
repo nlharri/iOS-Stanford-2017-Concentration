807782068e9fd194a33571cb487f10ea6024a210
László Harri Németh <nlharri@gmail.com>
2018-12-15 15:52:58 +0100

    initial commit of xcode project

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Németh László Harri on 2018. 12. 14..
+//  Copyright © 2018. Németh László Harri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
