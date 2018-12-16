a396fcde5c1919f32156429522cd12c61f31d0c2
László Harri Németh <nlharri@gmail.com>
2018-12-16 09:12:18 +0100

    enhancing with 4 cards and flip counter

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,14 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // property observer
+    var flipCount: Int = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    // ! means this property does not need initializer
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    // this would also be ok
+    //var emojiChoices = ["👻", "🙊", "🦊", "🐶"]
+    var emojiChoices: Array<String> = ["👻", "🙊", "🦊", "🐶"]
+    
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
+        // ! means "assume this optional is set, and grap the associated value
+        // if it is not set, it will crash!
+        //let cardNumber = cardButtons.firstIndex(of: sender)!
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print ("ERROR")
+        }
+    }
+    
+    @IBAction func touchMonkeyCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🙊", on: sender)
+        flipCount += 1
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
         } else {
             button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
